f7462341a1ff67aa4902e0a41133d933265d3051
sangyun <socio1425bsh@daum.net>
2020-11-18 00:07:32 +0000

    write code!

--- app.rb
+++ app.rb
@@ -40,6 +40,13 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    if @game.guess(letter) == ArgumentError
+      flash[:message]="Invalid guess"
+    elsif @game.guess(letter) == false
+      flash[:message]="You have already used that letter."
+    else
+      @game.guess(letter)
+    end
     redirect '/show'
   end
   
@@ -50,17 +57,32 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    @game.word_with_guesses
+    if @game.check_win_or_lose == :win
+      redirect '/win'
+    elsif @game.check_win_or_lose == :lose
+      redirect '/lose'
+    else
+      erb :show # You may change/remove this line
+    end
   end
   
   get '/win' do
     ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
-  end
+    if @game.check_win_or_lose == :win
+      erb:win
+    else
+      redirect '/show'
+    end
+  end  
   
   get '/lose' do
     ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    if @game.check_win_or_lose == :lose
+      erb :lose # You may change/remove this line
+    else
+      redirect '/show'
+    end
   end
   
 end
--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,5 +1,5 @@
 class HangpersonGame
-
+  
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
@@ -7,14 +7,60 @@ class HangpersonGame
 
   # def initialize()
   # end
-  
-  def initialize(word)
-    @word = word
+  attr_accessor :word, :guesses, :wrong_guesses
+   
+  def initialize(word)  
+    @word = "school"        #초기화 word:정답 
+    @guesses=''         #guesses : 예상한것 알파벳 목록
+    @wrong_guesses=''   #wrong_guesses : 틀린 알파벳 목
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
   #  => "cooking"   <-- some random word
+  
+  def guess(letter)
+    raise ArgumentError if letter=="" || letter==nil || !letter.match(/[A-Za-z]/)
+  
+    letter.downcase!
+    if @word.include? letter
+      if not @guesses.include? letter
+        @guesses += letter
+      else
+        return false
+      end
+    else
+      if not @wrong_guesses.include? letter
+        @wrong_guesses += letter
+      else
+        return false
+      end
+    end
+  end
+  
+  def word_with_guesses
+    display = ''
+    word = @word.split(//)
+    word.each do |letter|
+      if @guesses.include? letter
+        display += letter
+      else
+        display += '-'
+      end
+    end
+    return display
+  end
+  
+  def check_win_or_lose
+    if wrong_guesses.size >= 7
+      return :lose
+    elsif word_with_guesses == @word and wrong_guesses.size < 7 
+      return :win
+    else
+      return :play
+    end
+  end
+
   def self.get_random_word
     require 'uri'
     require 'net/http'
@@ -23,5 +69,5 @@ class HangpersonGame
       return http.post(uri, "").body
     }
   end
-
+  
 end
